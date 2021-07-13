e14602bd11805789cfc41116a04421ebe8795c8e
Pushpendra Kumar <79224572+Pushpendra6367@users.noreply.github.com>
2021-07-13 21:17:44 +0530

    Create String.sh

---
+++ String.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#   Copyright (C) 2021 PUSHPENDRA6367
+#
+#   Poject BLACKSHARKUSERBOT is free software: 
+#   you can redistribute it and/or modify it under the terms of the GNU General Public License 
+#   as published by the Free Software Foundation, either version 3 of the License, 
+#   or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful, 
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+#   See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along with this program. 
+#   If not, see <https://www.gnu.org/licenses/>.
+#
+
+
+echo """
+                      © 𝙻𝙴𝙶𝙴𝙽𝙳𝙰𝚁𝚈_𝙰𝙵_𝙱𝙻𝙰𝙲𝙺𝚂𝙷𝙰𝚁𝙺                  
+                    @black_shark_bot_support_group
+"""
+
+python3 string_session.py
+© 2021 GitHub, Inc.
